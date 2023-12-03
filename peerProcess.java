e0d0b49910fae75de711059191447ba92078f120
Robert Fontan <robertfontan@gmail.com>
2023-12-03 03:33:31 -0500

    Final update

--- peerProcess.java
+++ peerProcess.java
@@ -17,12 +17,10 @@ public class peerProcess {
             Scanner scanner = new Scanner(inputStream);
             while (scanner.hasNextLine()) {
                 String currLine = scanner.nextLine();
-                //System.out.println(currLine);
                 String[] parameters = currLine.split(" ");
                 Peer peer = new Peer();
                 peer.parseCommonConfig();
                 peer.parsePeerInfoConfig(Integer.parseInt(parameters[0]), parameters[1], Integer.parseInt(parameters[2]), Integer.parseInt(parameters[3]));
-                System.out.println("PeerID: " + peer.peerID + ", is being added to the peers hashtable");
                 peers.put(peer.peerID, peer);
             }
             scanner.close();
@@ -32,7 +30,6 @@ public class peerProcess {
     }
 
     public void startServer(Peer peer) {
-        System.out.println("Starting server thread for: " + peer.peerID);
         Server server = new Server(peer);
         Thread serverThread = new Thread(server);
         serverThread.start();
@@ -75,12 +72,8 @@ public class peerProcess {
 
         for (Map.Entry<Integer, Peer> entry : peers.entrySet()) {
             int currPeerID = entry.getKey();
-            System.out.println("peerProcess peerID: " + peers.get(peerID).peerID);
-            System.out.println("PeerID being iterated over: " + currPeerID);
-            
             // Assuming that the peerID's are in increasing order within the config files
             if (currPeerID < peerID) {
-                System.out.println("Client being created with host [" + peers.get(peerID).peerID + "] and target [" + entry.getValue().peerID + "]");
                 Client client = new Client(peers.get(peerID), entry.getValue());
                 client.connect();
                 logger.tcpConnect(peerID, currPeerID);
